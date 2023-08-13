a1ff3032495612e4d369a8881eec650e77a95ec9
axy-moon <augxy666@gmail.com>
2023-08-13 12:25:15 +0530

    Students List

--- components/Attendance.js
+++ components/Attendance.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View,  StyleSheet } from 'react-native'
 import React from 'react'
+import StudentsList from './commonComponents/StudentsList'
+import { Text, Layout } from '@ui-kitten/components'
 
 const AttendanceScreen = () => {
   return (
-    <View>
-      <Text>Attendance</Text>
-    </View>
+    <Layout style={styles.container}>
+      <Text category="h3">Attendance</Text>
+      <StudentsList/>
+    </Layout>
   )
 }
 
 export default AttendanceScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:60,
+  },
+});
--- components/commonComponents/StudentsList.js
+++ components/commonComponents/StudentsList.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { List, ListItem } from '@ui-kitten/components';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+import data from "../../roll.json"
 
 const StudentsList = () => {
-    const renderItem = ({ item, index }: { item: { title: string }; index: number }): React.ReactElement => (
-        <ListItem title={`${item.title} ${index + 1}`} />
-      );
+
+
+  const PresentIcon = () => (
+    <Icon name="checkmark-circle" size={26} color="#1F883D" />
+ );
+
+ const AbsentIcon = () => {
+  <Icon name="close-circle" size={26} color="#B31312" />
+ };
+    
+  const renderItem = ({ item }) => (
+    <ListItem
+      title={`${item.rollno}`}
+      description={`${item.name}`}
+      style={[styles.names,styles.shadowProp]}
+      accessoryRight={PresentIcon}
+    />
+  );
+
   return (
     <List
       style={styles.container}
@@ -16,3 +36,22 @@ const StudentsList = () => {
 }
 
 export default StudentsList
+
+const styles = StyleSheet.create({
+  container: {
+    padding:16,
+    backgroundColor:"#fff",
+  },
+  names:{
+    marginVertical:2,
+    borderRadius:6,
+    padding:2,
+    backgroundColor:"#F5F5F5"
+  },
+  shadowProp: {
+    shadowColor: '#000',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+});
