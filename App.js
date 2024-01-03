b64edf9b2e425eb074e66d0e53ee9eb7e15c7347
axy-moon <augxy666@gmail.com>
2024-01-03 18:22:22 +0530

    App Js

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { ApplicationProvider, Layout,  IconRegistry} from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import Login from './components/Login';
 import Home from './components/Home'
 import Main from './components/Main';
-import SquareButton from './components/commonComponents/SquareButton';
 import { StatusBar } from 'expo-status-bar';
 import Profile from './components/Profile';
 import Notifications from './components/Notifications';
 import Register from './components/Register';
 import AttendanceScreen from './components/Attendance';
-import SelectSimpleUsageShowcase from './components/commonComponents/SelectSimpleUsageShowcase';
 import Todo from './components/Todo';
 import SubjectDetails from './components/SubjectDetails';
 import DashboardPage from './components/DashboardPage';
 import ChatScreen from './components/ChatScreen';
-import LeftDrawer from './components/commonComponents/LeftDrawer';
-import Header from './components/Header';
+
 const { Navigator, Screen } = createStackNavigator();
 
 const App = () => (
@@ -29,23 +25,17 @@ const App = () => (
   <ApplicationProvider {...eva} theme={eva.light}>
     <NavigationContainer>
       <Navigator screenOptions={{headerShown: false}}>
-
         <Screen name='Main' component={Main}/>
-      <Screen name='Dashboard' component={DashboardPage}/>
-      <Screen name='Login' component={Login}/>
-      <Screen name='Details' component={SubjectDetails}/>
-      <Screen name='Todo' component={Todo}/>
-      <Screen name='Home' component={Home}/>
-      <Screen name='Register' component={Register}/>
+        <Screen name='Dashboard' component={DashboardPage}/>
+        <Screen name='Login' component={Login}/>
+        <Screen name='Details' component={SubjectDetails}/>
+        <Screen name='Todo' component={Todo}/>
+        <Screen name='Home' component={Home}/>
+        <Screen name='Register' component={Register}/>
         <Screen name='Profile' component={Profile}/>
         <Screen name='Notifications' component={Notifications}/>
-      <Screen name='Attendance' component={AttendanceScreen}/>
-      <Screen name='Chat' component={ChatScreen}/>
-
-        
-
-
-
+        <Screen name='Attendance' component={AttendanceScreen}/>
+        <Screen name='Chat' component={ChatScreen}/>
       </Navigator>
     </NavigationContainer>
     <StatusBar translucent />
